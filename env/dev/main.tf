42b6ca1fc671e80ecf4213538c9c86c5f3bcc031
飯嶋健憲 <iijima@pa-consul.co.jp>
2022-11-16 16:46:10 +0900

    location to var

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -16,13 +16,13 @@ provider "azurerm" {
 
 resource azurerm_resource_group rg {
   name     = var.resource_group_name
-  location = "westus2"
+  location = var.location
 }
 
 resource azurerm_virtual_network vnet {
   name                = "myTFVnet"
   address_space       = ["10.0.0.0/16"]
-  location            = "westus2"
+  location = var.location
   resource_group_name = azurerm_resource_group.rg.name
   
   tags = {
--- env/dev/variables.tf
+++ env/dev/variables.tf
@@ -1,3 +1,6 @@
 variable "resource_group_name" {
   default = "myTFResourceGroup"
+
+variable location {
+
 }

07cda08dbbe854410d522f33a1ec8cc289bc6cd1
飯嶋健憲 <iijima@pa-consul.co.jp>
2022-11-16 20:24:14 +0900

    module化

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -9,6 +9,16 @@ terraform {
 
   required_version = "~> 1.3.4"
 }
+terraform {
+ backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "salessquare"
+
+    workspaces {
+      name = "terraform"
+    }
+  }
+}
 
 provider "azurerm" {
   features {}
@@ -18,14 +28,8 @@ resource azurerm_resource_group rg {
   name     = var.resource_group_name
   location = var.location
 }
-
-resource azurerm_virtual_network vnet {
-  name                = "myTFVnet"
-  address_space       = ["10.0.0.0/16"]
-  location = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  
-  tags = {
-    Environment = "Terraform Getting Started"
-  }
+module "vnet" {
+  source = "../../modules/vnet"
+  subnet_name = "myTFVnet"
+  rg_name = azurerm_resource_group.rg.name
 }
---
+++ modules/vnet/variables.tf
@@ -0,0 +1,6 @@
+variable "subnet_name" {
+  
+}
+variable "rg_name" {
+  
+}

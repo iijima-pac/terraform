c6b21132ba8c128e8e633973b416c8132314a63b
飯嶋健憲 <iijima@pa-consul.co.jp>
2022-11-16 20:40:18 +0900

    RGをモジュール化

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -28,9 +28,13 @@ resource azurerm_resource_group rg {
   name     = var.resource_group_name
   location = var.location
 }
+module "resourcegroup" {
+  source = "../../modules/resourcegroup"
+  location = var.location
+}
 module "vnet" {
   source = "../../modules/vnet"
   subnet_name = "myTFVnet"
-  rg_name = azurerm_resource_group.rg.name
+  rg_name = module.resourcegroup.name
   location = var.location
 }

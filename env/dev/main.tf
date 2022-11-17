e5db2a13653585bc93d53adf18dccd22f449cded
飯嶋健憲 <iijima@pa-consul.co.jp>
2022-11-17 14:32:48 +0900

    subnet追加

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -34,3 +34,9 @@ module "vnet" {
   rg_name     = module.resourcegroup.name
   location    = var.location
 }
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = module.resourcegroup.name
+  virtual_network_name = module.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
